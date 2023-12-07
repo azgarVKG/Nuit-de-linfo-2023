5cf7cc646979a6aeaece6b1b5d4c40c443d1782e
BuildTools <unconfigured@null.spigotmc.org>
2023-12-07 21:29:13 +0100

    game update

--- game/game.py
+++ game/game.py
@@ -2,6 +2,7 @@ import pyxel, json
 
 class App:
     def __init__(self):
+        self.x = 0
         pyxel.init(160, 120, title="Name", fps=60, quit_key=pyxel.KEY_ESCAPE)
         pyxel.run(self.update, self.draw)
 
@@ -14,8 +15,11 @@ class App:
                 json.dump(data, file)
         if pyxel.btnp(pyxel.KEY_A):
             pyxel.quit()
+        if pyxel.btnp(pyxel.KEY_Z):
+            self.x += 1
 
     def draw(self):
+        pyxel.rect(self.x, 0, 10, 10, 5)
         pyxel.cls(0)
 
 App()
