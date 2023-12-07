34295265448a4d127773ce6a94a53c2d139654fc
thomas110106 <bourgougnonthomas@gmail.com>
2023-12-07 20:25:50 +0100

    ndl

--- database/db.sql
+++ database/db.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS Fakes(
     ID INT PRIMARY KEY,
     Info TEXT,
     Proof TEXT
-)
+);
 
 CREATE TABLE IF NOT EXISTS NoFakes(
     ID INT PRIMARY KEY,
@@ -10,4 +10,4 @@ CREATE TABLE IF NOT EXISTS NoFakes(
     Proof1 TEXT,
     Proof2 TEXT,
     Proof3 TEXT
-)
+);
