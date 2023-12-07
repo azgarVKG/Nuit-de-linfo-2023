573bb0b395e19a7aa5815ccfca8ec126e434abcf
CyllianDev <cyllian.rul08@gmail.com>
2023-12-07 21:20:09 +0100

    Update lib.py - db.sql

---
+++ database/db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE IF NOT EXISTS Fakes(
+    ID INT PRIMARY KEY,
+    Info TEXT,
+    Proof TEXT
+)
+
+CREATE TABLE IF NOT EXISTS NoFakes(
+    ID INT PRIMARY KEY,
+    Info TEXT,
+    Proof1 TEXT,
+    Proof2 TEXT,
+    Proof3 TEXT
+)
