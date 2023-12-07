31a7c49ee499454fcd7e1d9a3dbe649b96d077bd
Flowzyzz <147387521+Flowzyzz@users.noreply.github.com>
2023-12-07 22:15:40 +0200

    creation class joueur

--- game/jeu.py
+++ game/jeu.py
@@ -1,2 +1,8 @@
 import pyxel, math
 from random import randint
+
+class joueur:
+    def __init__(self, pseudo):
+        self.score = 0
+        self.mauvaise_reponse = 0
+        self.bonne_reponse = 0
