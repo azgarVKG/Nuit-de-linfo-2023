44ebbfcadb1c3affac074549b07194e50e3e2586
thomas110106 <134587240+thomas110106@users.noreply.github.com>
2023-12-07 21:48:46 +0100

    base de donné amélioration

--- database/db.sql
+++ database/db.sql
@@ -4,6 +4,14 @@ CREATE TABLE IF NOT EXISTS Fakes(
     Proof TEXT
 );
 
+INSERT INTO Fakes
+(ID, Info, Proof)
+VALUES
+(1, '',''),
+(2, '',''),
+(3, '',''),
+(4, '','');
+
 
 CREATE TABLE IF NOT EXISTS NoFakes(
     ID INT PRIMARY KEY,
@@ -11,4 +19,13 @@ CREATE TABLE IF NOT EXISTS NoFakes(
     Proof1 TEXT,
     Proof2 TEXT,
     Proof3 TEXT
+    FOREIGN KEY (ID)
 );
+
+INSERT INTO NoFakes
+(ID, Info, Proof1, Proof2, Proof3)
+VALUES
+(1, '','','',''),
+(2, '','','',''),
+(3, '','','',''),
+(4, '','','','');
